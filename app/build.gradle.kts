a886035c16a89ecf9be62866e776876b96b49a21
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-07-06 10:26:09 +0430

    feat(gradle): change gradle file to kts and config project

---
+++ app/build.gradle.kts
@@ -0,0 +1,116 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    // Kotlin Serialization
+    kotlin("plugin.serialization") version "1.7.0"
+    // Kotlin Parcelize
+    id("kotlin-parcelize")
+}
+
+android {
+    compileSdk = 32
+    buildToolsVersion = "32.0.0"
+
+    defaultConfig {
+        applicationId = "com.parsuomash.affogato"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.2.0"
+    }
+
+    packagingOptions {
+        resources.excludes.add("META-INF/AL2.0")
+        resources.excludes.add("META-INF/LGPL2.1")
+    }
+
+    kotlin {
+        sourceSets.debug {
+            kotlin.srcDir("build/generated/ksp/debug/kotlin")
+        }
+        sourceSets.release {
+            kotlin.srcDir("build/generated/ksp/release/kotlin")
+        }
+    }
+}
+
+dependencies {
+    // AndroidX ------------------------------------------------------------------------------------
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
+
+    // Compose -------------------------------------------------------------------------------------
+    val composeVersion = "1.2.0-rc03"
+
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-util:$composeVersion")
+
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.material:material-icons-core:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+
+    implementation("androidx.compose.animation:animation:$composeVersion")
+    implementation("androidx.compose.animation:animation-graphics:$composeVersion")
+
+    implementation("androidx.compose.runtime:runtime:$composeVersion")
+    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
+    implementation("androidx.compose.compiler:compiler:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0")
+
+    implementation("androidx.paging:paging-compose:1.0.0-alpha15")
+    implementation("androidx.activity:activity-compose:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    androidTestImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+
+    // Test ----------------------------------------------------------------------------------------
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+
+    // Instrumentation Test ------------------------------------------------------------------------
+    androidTestImplementation("junit:junit:4.13.2")
+    androidTestImplementation("com.google.truth:truth:1.1.3")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // Leakcanary ----------------------------------------------------------------------------------
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
+}
