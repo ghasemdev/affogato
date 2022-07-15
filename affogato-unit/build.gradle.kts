409c3dd62076f0dda61f7c2cb8516cb1ebf955ea
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-07-15 20:17:27 +0430

    test: add test for DimenValidator

--- affogato-unit/build.gradle.kts
+++ affogato-unit/build.gradle.kts
@@ -50,32 +50,7 @@ dependencies {
     val composeVersion = "1.2.0-rc03"
 
     implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.ui:ui-util:$composeVersion")
-
-    implementation("androidx.compose.foundation:foundation:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
-
-    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-
-    implementation("androidx.compose.runtime:runtime:$composeVersion")
-    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
-    implementation("androidx.compose.compiler:compiler:1.2.0")
-
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-    androidTestImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
-
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
-
-    // Test ----------------------------------------------------------------------------------------
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.1.3")
-
-    // Instrumentation Test ------------------------------------------------------------------------
-    androidTestImplementation("junit:junit:4.13.2")
-    androidTestImplementation("com.google.truth:truth:1.1.3")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
 }
 
 afterEvaluate {
