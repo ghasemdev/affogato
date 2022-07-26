ab60eff206c5125deb0d731fccc9794756a19c9e
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-07-26 16:36:04 +0430

    fix(unit): orientation in landscape mode

--- sample/src/main/java/com/parsuomash/affogato/MainActivity.kt
+++ sample/src/main/java/com/parsuomash/affogato/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.parsuomash.affogato
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -91,9 +93,12 @@ fun ScreenContent() {
 
 @Composable
 fun ScreenContent2(window: WindowSize) {
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
     val text by remember(window) {
         mutableStateOf(
-            when (window.width) {
+            when (if (isLandscape) window.height else window.width) {
                 WindowType.Compact -> "Compact"
                 WindowType.Medium -> "Medium"
                 WindowType.Expanded -> "Expanded"
