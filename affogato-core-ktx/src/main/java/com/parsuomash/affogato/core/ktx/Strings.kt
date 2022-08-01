9910acc165cc78b5697298f14353004539fdffc2
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-08-01 22:46:17 +0430

    feat(core-ktx): add strings extension

---
+++ affogato-core-ktx/src/main/java/com/parsuomash/affogato/core/ktx/Strings.kt
@@ -0,0 +1,355 @@
+@file:Suppress("unused")
+
+package com.parsuomash.affogato.core.ktx
+
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.util.*
+
+/**
+ * Return a [Boolean] if the string is alphanumeric.
+ *
+ * Example:
+ * ```Kotlin
+ * "YES".isAlphanumeric // true
+ * "1234".isAlphanumeric // true
+ * ```
+ * @since 1.1.0
+ */
+inline val String.isAlphanumeric: Boolean
+  get() = matches(Regex("\\w*"))
+
+/**
+ * Return a [Boolean] if the string is digit.
+ *
+ * Example:
+ * ```Kotlin
+ * "0123".isDigit // true
+ * "h34".isDigit // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.isDigit: Boolean
+  get() = matches(Regex("\\d*"))
+
+/**
+ * Return a [Boolean] if the string is a persian digit.
+ *
+ * Example:
+ * ```Kotlin
+ * "۱۵".isPersianDigit // true
+ * "15".isPersianDigit // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.isPersianDigit: Boolean
+  get() = matches(Regex("[۰-۹]*"))
+
+/**
+ * Return a [Boolean] if the string contains latin letter.
+ *
+ * Example:
+ * ```Kotlin
+ * "Hello".containsLatinLetter // true
+ * "Hello 1".containsLatinLetter // true
+ * "0 0".containsLatinLetter // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.containsLatinLetter: Boolean
+  get() = matches(Regex(".*[A-Za-z].*"))
+
+/**
+ * Return a [Boolean] if the string contains digit.
+ *
+ * Example:
+ * ```Kotlin
+ * "Hello".containsDigit // false
+ * "Hello 1".containsDigit // true
+ * "123".containsDigit // true
+ * ```
+ * @since 1.1.0
+ */
+inline val String.containsDigit: Boolean
+  get() = matches(Regex(".*[0-9].*"))
+
+/**
+ * Return a [Boolean] if the string contains persian digit.
+ *
+ * Example:
+ * ```Kotlin
+ * "Hello۲۱".containsPersianDigit // true
+ * "۲۱".containsPersianDigit // true
+ * "123".containsPersianDigit // false
+ * "hello".containsPersianDigit // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.containsPersianDigit: Boolean
+  get() = matches(Regex(".*[۰-۹].*"))
+
+/**
+ * Return a [Boolean] if the string contains both letter and digit.
+ *
+ * Example:
+ * ```Kotlin
+ * "Hello55".hasLettersAndDigits // true
+ * "123".hasLettersAndDigits // false
+ * "hello".hasLettersAndDigits // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.hasLettersAndDigits: Boolean
+  get() = containsLatinLetter && containsDigit
+
+/**
+ * Return a [Boolean] if the string is integer number.
+ *
+ * Example:
+ * ```Kotlin
+ * "123".isIntegerNumber // true
+ * "012".isIntegerNumber // true
+ * "12.5".isIntegerNumber // false
+ * "q".isIntegerNumber // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.isIntegerNumber: Boolean
+  get() = toIntOrNull() != null
+
+/**
+ * Return a [Boolean] if the string is decimal number.
+ *
+ * Example:
+ * ```Kotlin
+ * "123".isDecimalNumber // true
+ * "012".isDecimalNumber // true
+ * "12.5".isDecimalNumber // true
+ * "q".isDecimalNumber // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.isDecimalNumber: Boolean
+  get() = toDoubleOrNull() != null
+
+/**
+ * If the string is an HTTP URL (ie Starts with http:// or https://)
+ *
+ * Example:
+ * ```Kotlin
+ * "http://www.affogato.com".isHttp // true
+ * "http://www.affogato.com".isHttp // true
+ * "htt://www//".isHttp // false
+ * ```
+ * @since 1.1.0
+ */
+inline val String.isHttp: Boolean
+  get() = matches(Regex("(http|https)://[^\\s]*"))
+
+/**
+ * Translates a string into application/x-www-form-urlencoded format using a specific encoding
+ * scheme.
+ *
+ * Example:
+ * ```Kotlin
+ * "https://www.google.co.nz/?gfe_rd=cr&ei=dzbFV&gws_rd=ssl#q=java".urlEncode
+ * // https%3A%2F%2Fwww.google.co.nz%2F%3Fgfe_rd%3Dcr%26ei%3DdzbFV%26gws_rd%3Dssl%23q%3Djava
+ * ```
+ * @since 1.1.0
+ * @see URLEncoder
+ */
+inline val String.urlEncode: String get() = encodeToUrl()
+
+/**
+ * Decodes an application/x-www-form-urlencoded string using a specific encoding scheme.
+ *
+ * Example:
+ * ```Kotlin
+ * val s = "https%3A%2F%2Fwww.google.co.nz%2F%3Fgfe_rd%3Dcr%26ei%3DdzbFV%26gws_rd%3Dssl%23q%3Djava"
+ * s.urlDecode // https://www.google.co.nz/?gfe_rd=cr&ei=dzbFV&gws_rd=ssl#q=java
+ * ```
+ * @since 1.1.0
+ * @see URLDecoder
+ */
+inline val String.urlDecode: String get() = decodeToUrl()
+
+/**
+ * Get last path segment of the string.
+ *
+ * Example:
+ * ```Kotlin
+ * "https://google.com/chrome/".lastPathComponent // chrome
+ * "C:\\Windows\\Fonts\\font.ttf".lastPathComponent // font.ttf
+ * "/dev/null".lastPathComponent // null
+ * ```
+ * @since 1.1.0
+ */
+inline val String.lastPathComponent: String
+  get() {
+    var path = this
+    if (path.endsWith("/")) {
+      path = path.substring(0, path.lastIndex)
+    }
+    var index = path.lastIndexOf('/')
+    if (index < 0) {
+      if (path.endsWith("\\")) {
+        path = path.substring(0, path.lastIndex)
+      }
+      index = path.lastIndexOf('\\')
+      if (index < 0) {
+        return path
+      }
+    }
+    return path.substring(index + 1)
+  }
+
+/**
+ * Creates a string from all the elements separated using [separator] and using the given [prefix]
+ * and [postfix] if supplied.
+ *
+ * If the collection could be huge, you can specify a non-negative value of [limit], in which case
+ * only the first [limit] elements will be appended, followed by the [truncated] string
+ * (which defaults to "...").
+ *
+ * Example:
+ * ```Kotlin
+ * joinWith("H", "e", "l", "l", "o", separator = "") // Hello
+ * joinWith(1, 2, 3, 4, separator = ", ") // 1, 2, 3, 4
+ * ```
+ * @since 1.1.0
+ * @see joinToString
+ */
+fun <T> joinWith(
+  vararg params: T,
+  separator: CharSequence = " ",
+  prefix: CharSequence = "",
+  postfix: CharSequence = "",
+  limit: Int = -1,
+  truncated: CharSequence = "...",
+  transform: ((T) -> CharSequence)? = null
+): String = params.joinToString(
+  separator = separator,
+  prefix = prefix,
+  postfix = postfix,
+  limit = limit,
+  truncated = truncated,
+  transform = transform
+)
+
+/**
+ * Translates a string into application/x-www-form-urlencoded format using a specific encoding
+ * scheme.
+ *
+ * Example:
+ * ```Kotlin
+ * "https://www.google.co.nz/?gfe_rd=cr&ei=dzbFV&gws_rd=ssl#q=java".encodeToUrl()
+ * // https%3A%2F%2Fwww.google.co.nz%2F%3Fgfe_rd%3Dcr%26ei%3DdzbFV%26gws_rd%3Dssl%23q%3Djava
+ * ```
+ * @since 1.1.0
+ * @see URLEncoder
+ */
+fun String.encodeToUrl(charSet: String = "UTF-8"): String = URLEncoder.encode(this, charSet)
+
+/**
+ * Decodes an application/x-www-form-urlencoded string using a specific encoding scheme.
+ *
+ * Example:
+ * ```Kotlin
+ * val s = "https%3A%2F%2Fwww.google.co.nz%2F%3Fgfe_rd%3Dcr%26ei%3DdzbFV%26gws_rd%3Dssl%23q%3Djava"
+ * s.decodeToUrl() // https://www.google.co.nz/?gfe_rd=cr&ei=dzbFV&gws_rd=ssl#q=java
+ * ```
+ * @since 1.1.0
+ * @see URLDecoder
+ */
+fun String.decodeToUrl(charSet: String = "UTF-8"): String = URLDecoder.decode(this, charSet)
+
+/**
+ * append space in string builders
+ *
+ * Example:
+ * ```Kotlin
+ * val str = buildString {
+ *   append("Hello")
+ *   appendSpace()
+ *   append("World")
+ * } // "Hello World"
+ * ```
+ * @since 1.1.0
+ * @see StringBuilder
+ */
+fun StringBuilder.appendSpace(): StringBuilder = append(" ")
+
+/**
+ * Checks that the length of a string is valid for block execution
+ *
+ * Example:
+ * ```Kotlin
+ * var result: Boolean? = null
+ * "12345678".hasValidLength(8) { result = true }
+ * result // true
+ * ```
+ * @since 1.1.0
+ */
+inline fun String.hasValidLength(length: Int, block: () -> Unit) {
+  if (this.length <= length) block()
+}
+
+/**
+ * Returns a copy of this string having it's first letter title cased using the rules of the
+ * default locale, or the original string if it's empty or already starts with a title case letter.
+ *
+ * The title case of a character is usually the same as its upper case with several exceptions.
+ * The particular list of characters with the special title case form depends on the underlying
+ * platform.
+ *
+ * Example:
+ * ```Kotlin
+ * val name = "john"
+ * name.capitalize() // John
+ * ```
+ * @since 1.1.0
+ * @see kotlin.text.capitalize
+ */
+fun String.capitalize() = replaceFirstChar {
+  if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+}
+
+/**
+ * Returns the substring from the given range.
+ *
+ * Example:
+ * ```Kotlin
+ * val mainStr = "Interesting"
+ * val str = mainStr[2..8] // "teresti"
+ * ```
+ * @since 1.1.0
+ * @see substring
+ */
+operator fun String.get(range: IntRange): String = substring(range.first, range.last + 1)
+
+/**
+ * Returns the substring from the given range if exist otherwise return a null.
+ *
+ * Example:
+ * ```Kotlin
+ * "Hello".getOrNull(1..3) // ell
+ * "Hello".getOrNull(1..5) // null
+ * ```
+ * @since 1.1.0
+ * @see substring
+ */
+fun String.getOrNull(range: IntRange): String? = tryCatchNull { get(range) }
+
+/**
+ * Returns the substring from the given range.
+ *
+ * Example:
+ * ```Kotlin
+ * "Hello".getOrElse(1..3) { "Hi" } // ell
+ * "Hello".getOrElse(1..5) { "Hi" } // Hi
+ * ```
+ * @since 1.1.0
+ * @see substring
+ */
+fun String.getOrElse(range: IntRange, defaultValue: (range: IntRange) -> String): String =
+  tryCatchElse(elseBlock = { defaultValue(range) }) { get(range) }
---
+++ affogato-core-ktx/src/test/java/com/parsuomash/affogato/core/ktx/StringsKtTest.kt
@@ -0,0 +1,156 @@
+package com.parsuomash.affogato.core.ktx
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.jupiter.api.assertThrows
+
+class StringsKtTest {
+  @Test
+  fun `is alphanumeric`() {
+    assertThat("123".isAlphanumeric).isTrue()
+    assertThat("hello".isAlphanumeric).isTrue()
+    assertThat("YES".isAlphanumeric).isTrue()
+    assertThat("Hello".isAlphanumeric).isTrue()
+    assertThat("Hello1400".isAlphanumeric).isTrue()
+    assertThat("۵۰۵".isAlphanumeric).isFalse()
+    assertThat("سلام".isAlphanumeric).isFalse()
+  }
+
+  @Test
+  fun `is digit`() {
+    assertThat("7865".isDigit).isTrue()
+    assertThat("salam".isDigit).isFalse()
+    assertThat("hr56".isDigit).isFalse()
+    assertThat("الکی".isDigit).isFalse()
+  }
+
+  @Test
+  fun `is persian digit`() {
+    assertThat("۰۹۱۵۰۰۰۰۰۰۰".isPersianDigit).isTrue()
+    assertThat("09150000000".isPersianDigit).isFalse()
+    assertThat("۵۲s".isPersianDigit).isFalse()
+    assertThat("۵۲ض".isPersianDigit).isFalse()
+  }
+
+  @Test
+  fun `contains latin letter`() {
+    assertThat("Hello1400".containsLatinLetter).isTrue()
+    assertThat("Hello".containsLatinLetter).isTrue()
+    assertThat("hello".containsLatinLetter).isTrue()
+    assertThat("Helloسلام".containsLatinLetter).isTrue()
+    assertThat("0".containsLatinLetter).isFalse()
+    assertThat("ا".containsLatinLetter).isFalse()
+  }
+
+  @Test
+  fun `contains digit`() {
+    assertThat("Hello1400".containsDigit).isTrue()
+    assertThat("1234".containsDigit).isTrue()
+    assertThat("hello".containsDigit).isFalse()
+    assertThat("سلام".containsDigit).isFalse()
+    assertThat("۱۵۲۱۶۵".containsDigit).isFalse()
+  }
+
+  @Test
+  fun `contains persian digit`() {
+    assertThat("Hello۲۱".containsPersianDigit).isTrue()
+    assertThat("۲۱".containsPersianDigit).isTrue()
+    assertThat("hello".containsPersianDigit).isFalse()
+    assertThat("سلام5".containsPersianDigit).isFalse()
+    assertThat("125".containsPersianDigit).isFalse()
+  }
+
+  @Test
+  fun `has letters and digits`() {
+    assertThat("Hello55".hasLettersAndDigits).isTrue()
+    assertThat("hello".hasLettersAndDigits).isFalse()
+    assertThat("125".hasLettersAndDigits).isFalse()
+  }
+
+  @Test
+  fun `is integer number`() {
+    assertThat("123".isIntegerNumber).isTrue()
+    assertThat("012".isIntegerNumber).isTrue()
+    assertThat("12.5".isIntegerNumber).isFalse()
+    assertThat("q".isIntegerNumber).isFalse()
+  }
+
+  @Test
+  fun `is decimal number`() {
+    assertThat("123.56".isDecimalNumber).isTrue()
+    assertThat("127".isDecimalNumber).isTrue()
+    assertThat("012".isDecimalNumber).isTrue()
+    assertThat("012.65".isDecimalNumber).isTrue()
+    assertThat("q".isDecimalNumber).isFalse()
+  }
+
+  @Test
+  fun `is Http`() {
+    assertThat("http://www.jakode.com".isHttp).isTrue()
+    assertThat("https://www.jakode.com".isHttp).isTrue()
+    assertThat("htt://www//".isHttp).isFalse()
+  }
+
+  @Test
+  fun `url encoding`() {
+    val originalUrl = "https://www.google.co.nz/?gfe_rd=cr&ei=dzbFV&gws_rd=ssl#q=java"
+    val encodingUrl = originalUrl.urlEncode
+    println("encoding url: $encodingUrl")
+    assertThat(encodingUrl).isEqualTo(
+      "https%3A%2F%2Fwww.google.co.nz%2F%3Fgfe" +
+        "_rd%3Dcr%26ei%3DdzbFV%26gws_rd%3Dssl%23q%3Djava"
+    )
+
+    val decodedUrl = originalUrl.urlDecode
+    println("decoded url: $decodedUrl")
+    assertThat(decodedUrl).isEqualTo(originalUrl)
+  }
+
+  @Test
+  fun `last path component`() {
+    assertThat("https://google.com/chrome/".lastPathComponent).isEqualTo("chrome")
+    assertThat("C:\\Windows\\Fonts\\font.ttf".lastPathComponent).isEqualTo("font.ttf")
+    assertThat("C:\\Windows\\Fonts\\font.ttf\\".lastPathComponent).isEqualTo("font.ttf")
+    assertThat("".lastPathComponent).isEqualTo("")
+    assertThat("/dev/null".lastPathComponent).isEqualTo("null")
+  }
+
+  @Test
+  fun `join with`() {
+    assertThat(joinWith("H", "e", "l", "l", "o", separator = "")).isEqualTo("Hello")
+    assertThat(joinWith(1, 2, 3, 4, separator = ", ")).isEqualTo("1, 2, 3, 4")
+    assertThat(joinWith(1, 2, 3, 4)).isEqualTo("1 2 3 4")
+  }
+
+  @Test
+  fun `append space`() {
+    assertThat(buildString { appendSpace() }).isEqualTo(" ")
+  }
+
+  @Test
+  fun `string has valid length`() {
+    var result: Boolean? = null
+    "12345678".hasValidLength(8) { result = true }
+    assertThat(result).isTrue()
+  }
+
+  @Test
+  fun `string capitalize`() {
+    assertThat("hello".capitalize()).isEqualTo("Hello")
+    assertThat("Hello".capitalize()).isEqualTo("Hello")
+    assertThat("HELLO".capitalize()).isEqualTo("HELLO")
+  }
+
+  @Test
+  fun `substring with range`() {
+    assertThat("Hi"[0]).isEqualTo('H')
+    assertThat("Hello affogato core ktx"[10..20]).isEqualTo("gato core k")
+    assertThrows<StringIndexOutOfBoundsException> { ""[0..10] }
+
+    assertThat("Hello".getOrNull(1..3)).isEqualTo("ell")
+    assertThat("Hello".getOrNull(1..5)).isNull()
+
+    assertThat("Hello".getOrElse(1..3) { "Hi" }).isEqualTo("ell")
+    assertThat("Hello".getOrElse(1..5) { "Hi" }).isEqualTo("Hi")
+  }
+}
