dfa95e0bd00769bd6275018bee2b8e9fb0be708a
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-08-03 01:04:46 +0430

    chore(gradle): add junit 5

--- affogato-core-ktx/build.gradle.kts
+++ affogato-core-ktx/build.gradle.kts
@@ -9,11 +9,16 @@ java {
   targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+tasks.test {
+  useJUnitPlatform()
+}
+
 dependencies {
   // Test ------------------------------------------------------------------------------------------
-  testImplementation("junit:junit:4.13.2")
   testImplementation("io.kotlintest:kotlintest-core:3.4.2")
   testImplementation("com.google.truth:truth:1.1.3")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 }
 
 afterEvaluate {
