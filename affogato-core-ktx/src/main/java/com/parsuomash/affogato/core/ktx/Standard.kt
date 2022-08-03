192bc3940051b04516b1c310ffd9a622d82cbc9b
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-08-03 10:20:11 +0430

    feat(core-ktx): add counter function

--- affogato-core-ktx/src/main/java/com/parsuomash/affogato/core/ktx/Standard.kt
+++ affogato-core-ktx/src/main/java/com/parsuomash/affogato/core/ktx/Standard.kt
@@ -1,6 +1,170 @@
 package com.parsuomash.affogato.core.ktx
 
 /**
+ * Calculate count of each value by group in the string.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = "aba"
+ * counter(value) // { a = 2, b = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ */
+fun counter(content: String) = content.groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the Array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = arrayOf("a", "b", "a")
+ * counter(value) // { a = 2, b = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see arrayOf
+ */
+fun <T> counter(content: Array<T>) = content.groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the byte array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = byteArrayOf(1, 2, 1)
+ * counter(value) // { 1 = 2, 2 = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see byteArrayOf
+ */
+fun counter(content: ByteArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the char array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = charArrayOf('a', 'b', 'a')
+ * counter(value) // { a = 2, b = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see charArrayOf
+ */
+fun counter(content: CharArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the short array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = shortArrayOf(1, 2, 1)
+ * counter(value) // { 1 = 2, 2 = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see shortArrayOf
+ */
+fun counter(content: ShortArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the int array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = intArrayOf(1, 2, 1)
+ * counter(value) // { a = 2, b = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see intArrayOf
+ */
+fun counter(content: IntArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the long array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = longArrayOf(1L, 2L, 1L)
+ * counter(value) // { 1L = 2, 2L = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see longArrayOf
+ */
+fun counter(content: LongArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the float array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = floatArrayOf(1F, 2F, 1F)
+ * counter(value) // { 1F = 2, 2F = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see floatArrayOf
+ */
+fun counter(content: FloatArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the double array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = doubleArrayOf(1.0, 2.0, 1.0)
+ * counter(value) // { 1.0 = 2, 2.0 = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see doubleArrayOf
+ */
+fun counter(content: DoubleArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the boolean array.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = booleanArrayOf(false, true, false)
+ * counter(value) // { false 2, true = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see booleanArrayOf
+ */
+fun counter(content: BooleanArray) = content.toList().groupingBy { it }.eachCount()
+
+/**
+ * Calculate count of each value by group in the iterable.
+ *
+ * Example:
+ * ```Kotlin
+ * val value = listOf("a", "b", "a")
+ * counter(value) // { a = 2, b = 1 }
+ * ```
+ * @since 1.1.0
+ * @see groupingBy
+ * @see eachCount
+ * @see listOf
+ */
+fun <T> counter(content: Iterable<T>) = content.groupingBy { it }.eachCount()
+
+/**
  * when variable is null default value maintained.
  *
  * Example:
--- affogato-core-ktx/src/test/java/com/parsuomash/affogato/core/ktx/StandardKtTest.kt
+++ affogato-core-ktx/src/test/java/com/parsuomash/affogato/core/ktx/StandardKtTest.kt
@@ -3,12 +3,72 @@
 package com.parsuomash.affogato.core.ktx
 
 import com.google.common.truth.Truth.assertThat
+import com.parsuomash.affogato.core.ktx.collections.dequeOf
+import com.parsuomash.affogato.core.ktx.collections.linkedListOf
+import com.parsuomash.affogato.core.ktx.collections.queueOf
+import com.parsuomash.affogato.core.ktx.collections.stackOf
 import java.util.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class StandardKtTest {
+  @Nested
+  @DisplayName("counter")
+  inner class Counter {
+    @Test
+    @DisplayName("string")
+    fun string() {
+      assertThat(counter("")).isEqualTo(emptyMap<String, Int>())
+      assertThat(counter("abc")).isEqualTo(mapOf('a' to 1, 'b' to 1, 'c' to 1))
+    }
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    @Test
+    @DisplayName("arrays")
+    fun arrays() {
+      assertThat(counter(emptyArray<String>())).isEqualTo(emptyMap<String, Int>())
+      assertThat(counter(arrayOf(1, 2, 1))).isEqualTo(mapOf(1 to 2, 2 to 1))
+      assertThat(counter(arrayOf("a", "d", "d", "d"))).isEqualTo(mapOf("a" to 1, "d" to 3))
+
+      assertThat(counter(byteArrayOf(1, 2, 1)))
+        .isEqualTo(mutableMapOf(1.toByte() to 2, 2.toByte() to 1))
+      assertThat(counter(charArrayOf('a', 'b', 'a'))).isEqualTo(mapOf('a' to 2, 'b' to 1))
+      assertThat(counter(shortArrayOf(1, 2, 1)))
+        .isEqualTo(mapOf(1.toShort() to 2, 2.toShort() to 1))
+      assertThat(counter(intArrayOf(1, 2, 1))).isEqualTo(mapOf(1 to 2, 2 to 1))
+      assertThat(counter(uintArrayOf(1u, 2u, 1u))).isEqualTo(mapOf(1u to 2, 2u to 1))
+      assertThat(counter(longArrayOf(1L, 2L, 1L))).isEqualTo(mapOf(1L to 2, 2L to 1))
+      assertThat(counter(floatArrayOf(1F, 2F, 1F))).isEqualTo(mapOf(1F to 2, 2F to 1))
+      assertThat(counter(doubleArrayOf(1.0, 2.0, 1.0))).isEqualTo(mapOf(1.0 to 2, 2.0 to 1))
+      assertThat(counter(booleanArrayOf(false, true, false)))
+        .isEqualTo(mapOf(false to 2, true to 1))
+    }
+
+    @Test
+    @DisplayName("stack")
+    fun stack() {
+      assertThat(counter(stackOf("a", "a", "a", "b"))).isEqualTo(mapOf("a" to 3, "b" to 1))
+      assertThat(counter(stackOf(1, 2, 3, 2, 1, 3))).isEqualTo(mapOf(1 to 2, 2 to 2, 3 to 2))
+    }
+
+    @Test
+    @DisplayName("queues")
+    fun queues() {
+      assertThat(counter(queueOf("a", "a", "a", "b"))).isEqualTo(mapOf("a" to 3, "b" to 1))
+      assertThat(counter(dequeOf("a", "d", "d", "d"))).isEqualTo(mapOf("a" to 1, "d" to 3))
+    }
+
+    @Test
+    @DisplayName("lists")
+    fun lists() {
+      assertThat(counter(mutableListOf(1, 2, 1))).isEqualTo(mapOf(1 to 2, 2 to 1))
+      assertThat(counter(linkedListOf("a", "a", "a", "b"))).isEqualTo(mapOf("a" to 3, "b" to 1))
+      assertThat(counter(listOf("a", "a", "a", "b", 5, 2.3, null)))
+        .isEqualTo(mapOf("a" to 3, "b" to 1, 5 to 1, 2.3 to 1, null to 1))
+    }
+  }
+
   @Test
   @DisplayName("or default")
   fun orDefault() {
