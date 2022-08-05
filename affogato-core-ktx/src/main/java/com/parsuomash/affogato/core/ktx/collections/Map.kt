eae7a56cbeaa06e22d9d2b0e114da7a1661a3fd0
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-08-05 19:41:16 +0430

    feat(core-ktx): add map sorted functions

--- affogato-core-ktx/src/main/java/com/parsuomash/affogato/core/ktx/collections/Map.kt
+++ affogato-core-ktx/src/main/java/com/parsuomash/affogato/core/ktx/collections/Map.kt
@@ -21,3 +21,55 @@ inline fun <reified K, V> Map<K, V>.toHashMap(): HashMap<K, V> = HashMap(this)
  * @since 1.1.0
  */
 inline fun <reified K, V> Map<K, V>.toLinkedMap(): LinkedHashMap<K, V> = LinkedHashMap(this)
+
+/**
+ * Returns a map of all elements sorted by value.
+ *
+ * Example:
+ * ```Kotlin
+ * mapOf(5 to 1, 10 to 10, 1 to 0).sortedByValue() // {1:0, 5:1, 10:10}
+ * mapOf("11" to 1, "1" to 10, "a" to 0).sortedByValue() // {"1":10, "11":1, "a":0}
+ * ```
+ * @since 1.1.0
+ */
+inline fun <reified K, V> Map<K, V>.sortedByValue(): Map<K, V> =
+  toList().sortedBy { it.second.hashCode() }.toMap()
+
+/**
+ * Returns a map of all elements sorted by dec value.
+ *
+ * Example:
+ * ```Kotlin
+ * mapOf(5 to 1, 10 to 10, 1 to 0).sortedByValueDescending() // {10:10, 5:1, 1:0}
+ * mapOf("11" to 1, "1" to 10, "a" to 0).sortedByValueDescending() // {"a":0, "11":1, "1":10}
+ * ```
+ * @since 1.1.0
+ */
+inline fun <reified K, V> Map<K, V>.sortedByValueDescending(): Map<K, V> =
+  toList().sortedByDescending { it.second.hashCode() }.toMap()
+
+/**
+ * Example:
+ * ```Kotlin
+ * mapOf(1 to 0, 5 to 1, 10 to 10, 7 to 0).sortedByValue()
+ * // {1:0, 7:0, 5:1, 10:10}
+ * mapOf(1 to "a", 2 to "0", 3 to "11", 4 to "01", 5 to "1").sortedByValue()
+ * // {2:"0", 4:"01", 5:"1", 3:"11", 1:"a"}
+ * ```
+ * @since 1.1.0
+ */
+inline fun <reified K, V> Map<K, V>.sortedByKey(): Map<K, V> =
+  toList().sortedBy { it.first.hashCode() }.toMap()
+
+/**
+ * Example:
+ * ```Kotlin
+ * mapOf(1 to 0, 5 to 1, 10 to 10, 7 to 0).sortedByValueDescending()
+ * // {10:10, 5:1, 1:0, 7:0}
+ * mapOf(2 to "0", 3 to "11", 4 to "01", 5 to "1").sortedByValueDescending()
+ * // {3:"11", 5:"1", 4:"01", 2:"0"}
+ * ```
+ * @since 1.1.0
+ */
+inline fun <reified K, V> Map<K, V>.sortedByKeyDescending(): Map<K, V> =
+  toList().sortedByDescending { it.first.hashCode() }.toMap()
--- affogato-core-ktx/src/test/java/com/parsuomash/affogato/core/ktx/collections/MapKtTest.kt
+++ affogato-core-ktx/src/test/java/com/parsuomash/affogato/core/ktx/collections/MapKtTest.kt
@@ -21,4 +21,44 @@ internal class MapKtTest {
       assertThat(mapOf(1 to 1, 2 to 2).toLinkedMap()).isEqualTo(linkedMapOf(1 to 1, 2 to 2))
     }
   }
+
+  @Nested
+  @DisplayName("Sorting")
+  inner class Sorting {
+    @Test
+    @DisplayName("sorted by key")
+    fun sortedByKey() {
+      assertThat(mapOf(5 to 1, 10 to 10, 1 to 0).sortedByKey())
+        .isEqualTo(mapOf(1 to 0, 5 to 1, 10 to 10))
+      assertThat(mapOf("11" to 1, "1" to 10, "a" to 0).sortedByKey())
+        .isEqualTo(mapOf("1" to 10, "11" to 1, "a" to 0))
+    }
+
+    @Test
+    @DisplayName("sort by key dec")
+    fun sortedByKeyDec() {
+      assertThat(mapOf(5 to 1, 10 to 10, 1 to 0).sortedByKeyDescending())
+        .isEqualTo(mapOf(10 to 10, 5 to 1, 1 to 0))
+      assertThat(mapOf("11" to 1, "1" to 10, "a" to 0).sortedByKeyDescending())
+        .isEqualTo(mapOf("a" to 0, "1" to 10, "11" to 1))
+    }
+
+    @Test
+    @DisplayName("sorted by value")
+    fun sortedByValue() {
+      assertThat(mapOf(1 to 0, 5 to 1, 10 to 10, 7 to 0).sortedByValue())
+        .isEqualTo(mapOf(1 to 0, 7 to 0, 5 to 1, 10 to 10))
+      assertThat(mapOf(1 to "a", 2 to "0", 3 to "11", 4 to "01", 5 to "1").sortedByValue())
+        .isEqualTo(mapOf(2 to "0", 4 to "01", 5 to "1", 3 to "11", 1 to "a"))
+    }
+
+    @Test
+    @DisplayName("sort by value dec")
+    fun sortedByValueDec() {
+      assertThat(mapOf(1 to 0, 5 to 1, 10 to 10, 7 to 0).sortedByValueDescending())
+        .isEqualTo(mapOf(10 to 10, 5 to 1, 1 to 0, 7 to 0))
+      assertThat(mapOf(2 to "0", 3 to "11", 4 to "01", 5 to "1").sortedByValueDescending())
+        .isEqualTo(mapOf(3 to "11", 5 to "1", 4 to "01", 2 to "0"))
+    }
+  }
 }
