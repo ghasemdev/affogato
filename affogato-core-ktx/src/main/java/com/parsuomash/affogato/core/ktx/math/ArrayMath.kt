3bb7548213e04cf958cd698acbf775951e0e5de4
Ghasem Shirdel <shirdelghasem79@gmail.com>
2022-08-06 11:36:44 +0430

    refactor(core-ktx): separate collection math

---
+++ affogato-core-ktx/src/main/java/com/parsuomash/affogato/core/ktx/math/ArrayMath.kt
@@ -0,0 +1,356 @@
+@file:Suppress("TooManyFunctions", "ArrayPrimitive")
+
+package com.parsuomash.affogato.core.ktx.math
+
+import com.parsuomash.affogato.core.ktx.collections.sortedByValueDescending
+import com.parsuomash.affogato.core.ktx.counter
+
+// TODO v2.2.0 add trigonometric functions (sin, sinh, asin, asinh, cos, cosh,
+//  acos, acosh, tan, tanh, atan, atanh).
+
+// TODO 2.6.0 add math function for IntArray , FloatArray, DoubleArray and Long Array.
+
+/**
+ * Computes the median of the value [Array].
+ *
+ * Notice -> In pair list of values, the median is the two middle value.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1 , 5 , 3 , 9 , 0 , 4).median() // (3, 4)
+ * arrayOf(1 , 5 , 3 , 0 , 4).median() // (3 , null)
+ * ```
+ * @since 1.1.0
+ * @throws IllegalArgumentException if the [Array] is empty.
+ */
+inline fun <reified T> Array<T>.median(): Pair<T, T?> {
+  require(size >= 1) { "The array must have at least one element!!" }
+  val sortedArray = sortedBy { it.hashCode() }
+  return if (sortedArray.size % 2 == 0) {
+    Pair(sortedArray[size / 2 - 1], sortedArray[size / 2])
+  } else {
+    Pair(sortedArray[size / 2], null)
+  }
+}
+
+/**
+ * Computes the mode of the value [Array].
+ *
+ * Notice -> If all element repetition is equals all of them return as mode.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1, 1, 1, 2, 3, 4, 5, 5, 2).mode() // [1]
+ * arrayOf(1, 2, 3, 4, 5, 6, 7).mode() // [1, 2, 3, 4, 5, 6, 7]
+ * ```
+ * @since 1.1.0
+ */
+inline fun <reified T> Array<T>.mode(): List<T> {
+  val map = counter(this).sortedByValueDescending()
+  var repeat = -1
+  return buildList {
+    for ((key, value) in map) {
+      if (repeat == -1 || repeat == value) {
+        repeat = value
+        add(key)
+      }
+    }
+  }
+}
+
+/**
+ * Computes the standard division of the value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1, 2, 3, 4, 5, 6, 7).std() // 2.0
+ * arrayOf().std() // NAN
+ * ```
+ * @since 1.1.0
+ */
+inline fun <reified T : Number> Array<T>.std(): Double {
+  val numbers = map { it.toDouble() }
+  val mean = numbers.average()
+  var standardDeviation = 0.0
+
+  for (num in numbers) {
+    standardDeviation += (num - mean) pow 2
+  }
+  return kotlin.math.sqrt(standardDeviation / size)
+}
+
+/**
+ * Computes the variance of the value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1, 2, 3, 4, 5, 6, 7).variance() // 4.666666666666667
+ * arrayOf().variance() // NAN
+ * ```
+ * @since 1.1.0
+ */
+inline fun <reified T : Number> Array<T>.variance(): Double {
+  if (isEmpty()) return Double.NaN
+
+  val numbers = map { it.toDouble() }
+  val mean = numbers.average()
+  var standardDeviation = 0.0
+
+  for (num in numbers) {
+    standardDeviation += (num - mean) pow 2
+  }
+  return standardDeviation / (size - 1)
+}
+
+/**
+ * Raises this value to the power [array].
+ *
+ * Example:
+ * ```Kotlin
+ * 2 pow arrayOf(1, 2, 3) // [2.0, 4.0, 8.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.pow
+ */
+inline infix fun <reified T : Number> T.pow(array: Array<T>): Array<Double> =
+  array.map { this pow it }.toTypedArray()
+
+/**
+ * Raises this [Array] to the power [number].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1, 2, 3) pow 2 // [1.0, 4.0, 9.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.pow
+ */
+inline infix fun <reified T : Number> Array<T>.pow(number: T): Array<Double> =
+  map { it pow number }.toTypedArray()
+
+/**
+ * Computes the logarithm of the value x to the given [Array] bases.
+ *
+ * Example:
+ * ```Kotlin
+ * 1000 log arrayOf(1000, 100, 10, 1) // [1.0, 2.0 , 3.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.log
+ */
+inline infix fun <reified T : Number> T.log(array: Array<T>): Array<Double> =
+  array.map { this log it }.toTypedArray()
+
+/**
+ * Computes the logarithm of the [Array] values to the given base.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1000, 100, 10) log 10 // [3.0, 2.0 , 1.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.log
+ */
+inline infix fun <reified T : Number> Array<T>.log(number: T): Array<Double> =
+  map { it log number }.toTypedArray()
+
+/**
+ * Computes the common logarithm (base 10) of the value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1.0, 4.0, 9.0).log10() // [1.0, 2.0, 3.0, 4.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.log10
+ * @see kotlin.math.ln
+ */
+inline fun <reified T : Number> Array<T>.log10(): Array<Double> =
+  map { kotlin.math.log10(it.toDouble()) }.toTypedArray()
+
+/**
+ * Computes the binary logarithm (base 2) of the value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(2, 4, 8, 16).log2() // [1.0, 2.0, 3.0, 4.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.log2
+ * @see kotlin.math.ln
+ */
+inline fun <reified T : Number> Array<T>.log2(): Array<Double> =
+  map { kotlin.math.log2(it.toDouble()) }.toTypedArray()
+
+/**
+ * Computes the natural logarithm (base E) of the value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1.0, 4.0, 9.0).ln() // [0.0, 1.38, 2.19]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.ln
+ */
+inline fun <reified T : Number> Array<T>.ln(): Array<Double> =
+  map { kotlin.math.ln(it.toDouble()) }.toTypedArray()
+
+/**
+ * Rounds the given value [Array] towards the closest integer with ties
+ * rounded towards even integer.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(31.62F, 10F, 3.16F).round() // [32F, 10F, 3F]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.round
+ */
+fun Array<Float>.round(): Array<Float> = map { kotlin.math.round(it) }.toTypedArray()
+
+/**
+ * Rounds the given value [Array] towards the closest integer with ties
+ * rounded towards even integer.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(31.622776601683793, 10.0, 3.1622776601683795).round() // [32.0, 10.0, 3.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.round
+ */
+fun Array<Double>.round(): Array<Double> = map { kotlin.math.round(it) }.toTypedArray()
+
+/**
+ * Computes the positive square root of the value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1.0, 4.0, 9.0).sqrt() // [1.0, 2.0, 3.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.sqrt
+ */
+inline fun <reified T : Number> Array<T>.sqrt(): Array<Double> =
+  map { kotlin.math.sqrt(it.toDouble()) }.toTypedArray()
+
+/**
+ * Returns the absolute value of the given value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(-1, 2, 0, -6).abs() // [1, 2, 0, 6]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.abs
+ */
+@JvmName("absInt")
+fun Array<Int>.abs(): Array<Int> = map { kotlin.math.abs(it) }.toTypedArray()
+
+/**
+ * Returns the absolute value of the given value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(-1F, 2F, 0F, -6F).abs() // [1F, 2F, 0F, 6F]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.abs
+ */
+@JvmName("absFloat")
+fun Array<Float>.abs(): Array<Float> = map { kotlin.math.abs(it) }.toTypedArray()
+
+/**
+ * Returns the absolute value of the given value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(-1.0, 2.0, 0.0, -6.0).abs() // [1.0, 2.0, 0.0, 6.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.abs
+ */
+@JvmName("absDouble")
+fun Array<Double>.abs(): Array<Double> = map { kotlin.math.abs(it) }.toTypedArray()
+
+/**
+ * Returns the absolute value of the given value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(-1L, 2L, 0L, -6L).abs() // [1L, 2L, 0L, 6L]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.abs
+ */
+@JvmName("absLong")
+fun Array<Long>.abs(): Array<Long> = map { kotlin.math.abs(it) }.toTypedArray()
+
+/**
+ * Rounds the given value [Array] to an integer towards positive infinity.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1.1, 1.4999999999999998, 2.9999999999999996, 2.6).ceil() // [2.0, 2.0, 3.0, 3.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.ceil
+ */
+inline fun <reified T : Number> Array<T>.ceil(): Array<Double> =
+  map { kotlin.math.ceil(it.toDouble()) }.toTypedArray()
+
+/**
+ * Rounds the given value [Array] to an integer towards negative infinity.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1.1, 1.4999999999999998, 2.9999999999999996, 2.6).floor() // [1.0, 1.0, 2.0, 2.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.floor
+ */
+inline fun <reified T : Number> Array<T>.floor(): Array<Double> =
+  map { kotlin.math.floor(it.toDouble()) }.toTypedArray()
+
+/**
+ * Returns the sign of the given value [Array]:
+ * - -1.0 if the value is negative,
+ * - zero if the value is zero,
+ * - 1.0 if the value is positive
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(-1.0, 2.0, 0.0, -6.0).sign() // [-1.0, 1.0, 0.0, -1.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.sign
+ */
+inline fun <reified T : Number> Array<T>.sign(): Array<Double> =
+  map { kotlin.math.sign(it.toDouble()) }.toTypedArray()
+
+/**
+ * Computes Euler's number e raised to the power of the value [Array].
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1, 2, 3, 4).exp()
+ * // [2.718281828459045, 7.38905609893065, 20.085536923187668, 54.598150033144236]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.exp
+ */
+inline fun <reified T : Number> Array<T>.exp(): Array<Double> =
+  map { kotlin.math.exp(it.toDouble()) }.toTypedArray()
+
+/**
+ * Rounds the given value [Array] to an integer towards zero.
+ *
+ * Example:
+ * ```Kotlin
+ * arrayOf(1.1, 1.49998, 2.99996, 2.6).truncate() // [1.0, 1.0, 2.0, 2.0]
+ * ```
+ * @since 1.1.0
+ * @see kotlin.math.truncate
+ */
+inline fun <reified T : Number> Array<T>.truncate(): Array<Double> =
+  map { kotlin.math.truncate(it.toDouble()) }.toTypedArray()
--- affogato-core-ktx/src/test/java/com/parsuomash/affogato/core/ktx/math/CollectionsMathKtTest.kt
+++ affogato-core-ktx/src/test/java/com/parsuomash/affogato/core/ktx/math/CollectionsMathKtTest.kt
@@ -6,19 +6,10 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-internal class CollectionsKtTest {
+internal class CollectionsMathKtTest {
   @Nested
   @DisplayName("Median")
   inner class Median {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1, 5, 3, 9, 0, 4).median()).isEqualTo(Pair(3, 4))
-      assertThat(arrayOf(1, 5, 3, 0, 4).median()).isEqualTo(Pair(3, null))
-      assertThat(arrayOf("hi", "hello", "f ", "d", "a").median()).isEqualTo(Pair("f ", null))
-      assertThrows<IllegalArgumentException> { emptyArray<Float>().median() }
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
@@ -33,15 +24,6 @@ internal class CollectionsKtTest {
   @DisplayName("Mode")
   inner class Mode {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1, 1, 1, 2, 3, 4, 5, 5, 2).mode()).isEqualTo(listOf(1))
-      assertThat(arrayOf(1, 2, 3, 4, 5, 6, 7).mode()).isEqualTo(listOf(1, 2, 3, 4, 5, 6, 7))
-      assertThat(arrayOf("a", "a", "a", "b").mode()).isEqualTo(listOf("a"))
-      assertThat(emptyArray<Long>().mode()).isEqualTo(listOf<Long>())
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(1, 1, 1, 2, 3, 4, 5, 5, 2).mode()).isEqualTo(listOf(1))
@@ -55,14 +37,6 @@ internal class CollectionsKtTest {
   @DisplayName("Std")
   inner class STD {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1, 1, 1, 2, 3, 4, 5, 5, 2).std()).isEqualTo(1.5634719199411433)
-      assertThat(arrayOf(1, 2, 3, 4, 5, 6, 7).std()).isEqualTo(2)
-      assertThat(emptyArray<Long>().std()).isEqualTo(Double.NaN)
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(1, 1, 1, 2, 3, 4, 5, 5, 2).std()).isEqualTo(1.5634719199411433)
@@ -74,14 +48,6 @@ internal class CollectionsKtTest {
   @Nested
   @DisplayName("Variance")
   inner class Variance {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1, 1, 1, 2, 3, 4, 5, 5, 2).variance()).isEqualTo(2.75)
-      assertThat(arrayOf(1, 2, 3, 4, 5, 6, 7).variance()).isEqualTo(4.666666666666667)
-      assertThat(emptyArray<Long>().variance()).isEqualTo(Double.NaN)
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
@@ -95,13 +61,6 @@ internal class CollectionsKtTest {
   @DisplayName("Power")
   inner class Power {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1, 2, 3, 4, 5) pow 2).isEqualTo(arrayOf(1.0, 4.0, 9.0, 16.0, 25.0))
-      assertThat(2 pow arrayOf(1, 2, 3, 4, 5)).isEqualTo(arrayOf(2.0, 4.0, 8.0, 16.0, 32.0))
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(1, 2, 3, 4, 5) pow 2).isEqualTo(listOf(1.0, 4.0, 9.0, 16.0, 25.0))
@@ -112,15 +71,6 @@ internal class CollectionsKtTest {
   @Nested
   @DisplayName("Logaritm")
   inner class Logaritm {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(10, 100, 1_000, 10_000) log 10)
-        .isEqualTo(arrayOf(1.0, 2.0, 2.9999999999999996, 4.0))
-      assertThat(1_000 log arrayOf(1_000, 100, 10))
-        .isEqualTo(arrayOf(1.0, 1.4999999999999998, 2.9999999999999996))
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
@@ -135,15 +85,6 @@ internal class CollectionsKtTest {
   @DisplayName("Log10")
   inner class Log10 {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(10, 100, 1_000, 10_000).log10())
-        .isEqualTo(arrayOf(1.0, 2.0, 3.0, 4.0))
-      assertThat(arrayOf(1_000, 100, 10).log10())
-        .isEqualTo(arrayOf(3.0, 2.0, 1.0))
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(10, 100, 1_000, 10_000).log10())
@@ -157,15 +98,6 @@ internal class CollectionsKtTest {
   @DisplayName("Log2")
   inner class Log2 {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(2, 4, 8, 16).log2())
-        .isEqualTo(arrayOf(1.0, 2.0, 3.0, 4.0))
-      assertThat(arrayOf(16, 8, 4).log2())
-        .isEqualTo(arrayOf(4.0, 3.0, 2.0))
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(2, 4, 8, 16).log2())
@@ -178,22 +110,6 @@ internal class CollectionsKtTest {
   @Nested
   @DisplayName("Ln")
   inner class Ln {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(10, 100, 1_000, 10_000).ln())
-        .isEqualTo(
-          arrayOf(
-            2.302585092994046,
-            4.605170185988092,
-            6.907755278982137,
-            9.210340371976184
-          )
-        )
-      assertThat(arrayOf(1_000, 100, 10).ln())
-        .isEqualTo(arrayOf(6.907755278982137, 4.605170185988092, 2.302585092994046))
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
@@ -215,15 +131,6 @@ internal class CollectionsKtTest {
   @DisplayName("Round")
   inner class Round {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(3.1622776601683795, 10.0, 31.622776601683793, 100.0).round())
-        .isEqualTo(arrayOf(3.0, 10.0, 32.0, 100.0))
-      assertThat(arrayOf(31.622776601683793, 10.0, 3.1622776601683795).round())
-        .isEqualTo(arrayOf(32.0, 10.0, 3.0))
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(3.1622776601683795, 10.0, 31.622776601683793, 100.0).round())
@@ -236,15 +143,6 @@ internal class CollectionsKtTest {
   @Nested
   @DisplayName("Sqrt")
   inner class Sqrt {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(10, 100, 1_000, 10_000).sqrt())
-        .isEqualTo(arrayOf(3.1622776601683795, 10.0, 31.622776601683793, 100.0))
-      assertThat(arrayOf(1_000, 100, 10).sqrt())
-        .isEqualTo(arrayOf(31.622776601683793, 10.0, 3.1622776601683795))
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
@@ -259,15 +157,6 @@ internal class CollectionsKtTest {
   @DisplayName("Abs")
   inner class Abs {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(-1, 2, 0, -6).abs()).isEqualTo(arrayOf(1, 2, 0, 6))
-      assertThat(arrayOf(-1F, 2F, 0F, -6F).abs()).isEqualTo(arrayOf(1F, 2F, 0F, 6F))
-      assertThat(arrayOf(-1.0, 2.0, 0.0, -6.0).abs()).isEqualTo(arrayOf(1.0, 2.0, 0.0, 6.0))
-      assertThat(arrayOf(-1L, 2L, 0L, -6L).abs()).isEqualTo(arrayOf(1L, 2L, 0L, 6L))
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(-1, 2, 0, -6).abs()).isEqualTo(listOf(1, 2, 0, 6))
@@ -280,15 +169,6 @@ internal class CollectionsKtTest {
   @Nested
   @DisplayName("Ceil")
   inner class Ceil {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1.1, 1.4999999999999998, 2.9999999999999996, 2.6).ceil())
-        .isEqualTo(arrayOf(2.0, 2.0, 3.0, 3.0))
-      assertThat(arrayOf(1.1F, 1.49F, 2.99F, 2.6F).ceil())
-        .isEqualTo(arrayOf(2.0, 2.0, 3.0, 3.0))
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
@@ -303,15 +183,6 @@ internal class CollectionsKtTest {
   @DisplayName("Floor")
   inner class Floor {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1.1, 1.4999999999999998, 2.9999999999999996, 2.6).floor())
-        .isEqualTo(arrayOf(1.0, 1.0, 2.0, 2.0))
-      assertThat(arrayOf(1.1F, 1.49F, 2.99F, 2.6F).floor())
-        .isEqualTo(arrayOf(1.0, 1.0, 2.0, 2.0))
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(1.1, 1.4999999999999998, 2.9999999999999996, 2.6).floor())
@@ -325,13 +196,6 @@ internal class CollectionsKtTest {
   @DisplayName("Sign")
   inner class Sign {
     @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(-1F, 2F, 0F, -6F).sign()).isEqualTo(arrayOf(-1.0, 1.0, 0.0, -1.0))
-      assertThat(arrayOf(-1.0, 2.0, 0.0, -6.0).sign()).isEqualTo(arrayOf(-1.0, 1.0, 0.0, -1.0))
-    }
-
-    @Test
     @DisplayName("Iterable")
     fun iterable() {
       assertThat(listOf(-1F, 2F, 0F, -6F).sign()).isEqualTo(listOf(-1.0, 1.0, 0.0, -1.0))
@@ -342,20 +206,6 @@ internal class CollectionsKtTest {
   @Nested
   @DisplayName("Exp")
   inner class Exp {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1, 2, 3, 4).exp())
-        .isEqualTo(
-          arrayOf(
-            2.718281828459045,
-            7.38905609893065,
-            20.085536923187668,
-            54.598150033144236
-          )
-        )
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
@@ -374,13 +224,6 @@ internal class CollectionsKtTest {
   @Nested
   @DisplayName("Truncate")
   inner class Truncate {
-    @Test
-    @DisplayName("Array")
-    fun array() {
-      assertThat(arrayOf(1.1, 1.49998, 2.99996, 2.6).truncate())
-        .isEqualTo(arrayOf(1.0, 1.0, 2.0, 2.0))
-    }
-
     @Test
     @DisplayName("Iterable")
     fun iterable() {
