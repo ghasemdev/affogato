bf6df06d34c2982dd912601974a031d78d9cff2e
Ghasem Shirdel <shirdelghasem79@gmail.com>
2023-09-17 22:46:16 +0330

    chore: update dependencies

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -2,11 +2,11 @@ object Configuration {
   /** com.parsuomash.tick */
   const val APPLICATION_ID = "com.parsuomash.affogato"
 
-  /** 33 */
-  const val COMPILE_SDK = 33
+  /** 34 */
+  const val COMPILE_SDK = 34
 
-  /** 32.0.0 */
-  const val BUILD_TOOLS_VERSION = "33.0.2"
+  /** 34.0.0 */
+  const val BUILD_TOOLS_VERSION = "34.0.0"
 
   /** 21 */
   const val MIN_SDK = 21
@@ -14,7 +14,7 @@ object Configuration {
   /** 33 */
   const val TARGET_SDK = 33
 
-  /** 0.3.0 */
+  /** 1.0.0 */
   const val VERSION_NAME = "1.0.0"
 
   val VERSION_CODE = getVersionCode()
