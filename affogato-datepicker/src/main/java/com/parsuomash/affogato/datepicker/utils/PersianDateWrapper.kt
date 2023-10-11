541f09041a8b6f41b5c89241fce7a0081286081e
Ghasem Shirdel <shirdelghasem79@gmail.com>
2023-10-11 21:21:44 +0330

    refactor: change to data class

--- affogato-datepicker/src/main/java/com/parsuomash/affogato/datepicker/utils/PersianDateWrapper.kt
+++ affogato-datepicker/src/main/java/com/parsuomash/affogato/datepicker/utils/PersianDateWrapper.kt
@@ -4,5 +4,4 @@ import androidx.compose.runtime.Immutable
 import saman.zamani.persiandate.PersianDate
 
 @Immutable
-@JvmInline
-value class PersianDateWrapper(val value: PersianDate = PersianDate())
+data class PersianDateWrapper(val value: PersianDate = PersianDate())
