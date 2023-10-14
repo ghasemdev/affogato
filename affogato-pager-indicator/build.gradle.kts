c5ecee0c19e950acab524da8527361db95178fa7
Ghasem Shirdel <shirdelghasem79@gmail.com>
2023-10-14 18:30:59 +0330

    feat: add new module

---
+++ affogato-pager-indicator/build.gradle.kts
@@ -0,0 +1,50 @@
+import Configuration.MIN_SDK
+
+plugins {
+  alias(libs.plugins.android.library)
+  id(libs.plugins.maven.publish.get().pluginId)
+}
+
+apply {
+  from("$rootDir/android-library-build.gradle")
+}
+
+android {
+  namespace = "com.parsuomash.affogato.pager.indicator"
+
+  defaultConfig {
+    minSdk = MIN_SDK
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+  }
+  lint {
+    baseline = file("$rootDir/config/lint/lint-baseline.xml")
+  }
+}
+
+dependencies {
+  // Compose -------------------------------------------------------------------------------------
+  implementation(libs.compose.ui)
+  implementation(libs.compose.foundation)
+  implementation(libs.compose.material)
+  implementation(libs.compose.ui.tooling)
+  debugImplementation(libs.compose.ui.tooling)
+}
+
+afterEvaluate {
+  publishing {
+    publications {
+      create<MavenPublication>("release") {
+        groupId = "com.parsuomash.affogato"
+        artifactId = "affogato-pager-indicator"
+        version = libs.versions.affogato.get()
+
+        from(components["release"])
+      }
+    }
+  }
+}
